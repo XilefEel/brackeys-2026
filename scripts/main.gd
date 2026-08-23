71b8cf12abd2988223a3422a78022bfd071b3f30
Felix Lee <felixlee175@gmail.com>
2026-08-23 22:54:14 +0700

    added basic view switching

---
+++ scripts/main.gd
@@ -0,0 +1,44 @@
+extends Control
+
+enum RoomViews {
+	MAIN_ROOM,
+	DOOR_1,
+	DOOR_2,
+	DOOR_3,
+	GUARD_1,
+	GUARD_2,
+	GUARD_3,
+}
+
+@onready var views: Dictionary = {
+	RoomViews.MAIN_ROOM: $RoomView,
+	RoomViews.DOOR_1: $Door1View,
+	RoomViews.DOOR_2: $Door2View,
+	RoomViews.DOOR_3: $Door3View,
+	RoomViews.GUARD_1: $Guard1View,
+	RoomViews.GUARD_2: $Guard2View,
+	RoomViews.GUARD_3: $Guard3View
+}
+
+var current_view: RoomViews = RoomViews.MAIN_ROOM
+
+func _ready() -> void:
+	switch_view(RoomViews.MAIN_ROOM)
+
+
+func switch_view(target_view: RoomViews) -> void:
+	current_view = target_view
+
+	for view_type in views.keys():
+		views[view_type].visible = (view_type == target_view)
+
+
+func _on_to_door_1_pressed() -> void: switch_view(RoomViews.DOOR_1)
+func _on_to_door_2_pressed() -> void: switch_view(RoomViews.DOOR_2)
+func _on_to_door_3_pressed() -> void: switch_view(RoomViews.DOOR_3)
+
+func _on_to_guard_1_pressed() -> void: switch_view(RoomViews.GUARD_1)
+func _on_to_guard_2_pressed() -> void: switch_view(RoomViews.GUARD_2)
+func _on_to_guard_3_pressed() -> void: switch_view(RoomViews.GUARD_3)
+
+func _on_go_back_pressed() -> void: switch_view(RoomViews.MAIN_ROOM)
